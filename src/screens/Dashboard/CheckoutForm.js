bf953cb53e203d0d819b3c40ca917b70507fece5
lohidhasan <lohi.s@technoduce.com>
2023-09-28 17:03:31 +0530

    payment return url

--- src/screens/Dashboard/CheckoutForm.js
+++ src/screens/Dashboard/CheckoutForm.js
@@ -24,7 +24,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: `${window.location.origin}/completion`,
+        return_url: `https://6515634197ad080008f5f826--snazzy-lolly-c7aa33.netlify.app/completion`,
       },
     });
 
